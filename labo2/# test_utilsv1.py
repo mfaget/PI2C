a5b3a47c42cf549b8d24de5b48c75884ca3c01c1
Matthieu Faget <20517@ecam.be>
2021-03-06 15:52:03 +0100

    travis

---
+++ labo2/# test_utilsv1.py
@@ -0,0 +1,18 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 8, 2018
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_labo2exp(self):
+        self.assertEqual(1 , 2)
+        pass
+    
+    
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
